195cf3e85f30e808af8ea7c7f773b0228210145f
liyang <liyang5266@gmail.com>
2017-05-03 16:51:39 +0800

    encode

--- src/test/Encode.java
+++ src/test/Encode.java
@@ -6,12 +6,13 @@ import java.nio.charset.Charset;
 public class Encode {
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		  String defaultCharsetName=Charset.defaultCharset().displayName();   
-	        System.out.println("defaultCharsetName:"+defaultCharsetName);  
+	    System.out.println("defaultCharsetName:"+defaultCharsetName);  
 		String hz = new String("汉字".getBytes(), "ISO-8859-1");
 		System.err.println(hz);
 		String hz2 = new String(hz.getBytes("ISO-8859-1"), "utf-8");
 		System.err.println(hz2);
 		
+//		System.err.println(System.getProperties());
 //		byte[] b_gbk = "中".getBytes("GBK"); 
 //		byte[] b_utf8 = "中".getBytes("UTF-8"); 
 //		byte[] b_iso88591 = "中".getBytes("ISO8859-1"); 
